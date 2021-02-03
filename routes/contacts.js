22a0b8781c0a995433b84e6cea3326397a9fe2e5
TeylerL <46732911+TaylerL@users.noreply.github.com>
2021-02-03 16:47:03 -0500

    Authentication and beginning of contacts

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,18 +1,31 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../middleware/auth');
+const { check, validationResult } = require('express-validator');
+
+const User = require('../models/User');
+const Contact = require('../models/Contacts');
 
 // @route   GET api/contacts
 // @desc    Get all user's contacts
 // @access  Private
-router.get('/', (req, res) => {
-  res.send('Get users contacts');
+router.get('/', auth, async (req, res) => {
+  try {
+    const contacts = await Contact.find({ user: req.user.id }).sort({
+      date: -1
+    });
+    res.json(contacts);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
 });
 
 // @route   POST api/contacts
 // @desc    Add new contact
 // @access  Private
-router.get('/', (req, res) => {
-  res.send('Add contact');
+router.post('/', (req, res) => {
+  res.send('Get users contacts');
 });
 
 // @route   PUT api/contacts
