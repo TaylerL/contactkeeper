afed0fe4e4a582b6be7fe8bcb643bcba7b2a550f
TeylerL <46732911+TaylerL@users.noreply.github.com>
2021-02-09 02:19:07 -0500

    React UI complete

---
+++ client/src/components/pages/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export const About = () => {
+  return (
+    <div>
+      <h1>About This App</h1>
+      <p className='my-1'>This is a fullstack React app for keeping contacts</p>
+      <p className='bg-dar p'>
+        <strong>Version:</strong> 1.0.0
+      </p>
+    </div>
+  );
+};
+
+export default About;
